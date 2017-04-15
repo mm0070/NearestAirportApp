2e902b46392b7aeca7b25f79c2eeff7b72dcdb77
Michał Mildner <mm0070@DeathStar.local>
2017-04-15 23:29:29 +0100

    general cleanup, pre frontend

--- nearestAirportApp/urls.py
+++ nearestAirportApp/urls.py
@@ -1,10 +1,7 @@
 from django.conf.urls import url
-
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^search/', views.search, name='search'),
 ]
-
-# url(r'^(?P<icao>[\w.@+-]+)/$', views.detail, name='detail'),
--- nearestAirportApp/views.py
+++ nearestAirportApp/views.py
@@ -1,13 +1,8 @@
 from .models import Airportslist
 from django.http import HttpResponse
-from django.template import loader
 from django.shortcuts import render
 import json
-from django.core import serializers
 from geopy.distance import great_circle
-
-from django.shortcuts import render_to_response
-from django.template import RequestContext
 
 
 def search(request):
@@ -38,36 +33,28 @@ def search(request):
 
     print(lat)
     print(lon)
-    lat_lon = "RESPONSE FROM THE SERVER"
 
     print(nearest_airport)
-    return HttpResponse(json.dumps({'lat_lon': lat_lon}), content_type="application/json")
+
+    icao = nearest_airport[0]
+    type = nearest_airport[1]
+    name = nearest_airport[2]
+    lat = nearest_airport[3]
+    lon = nearest_airport[4]
+    elevation = nearest_airport[5]
+    country = nearest_airport[6]
+    region = nearest_airport[7]
+
+    return HttpResponse(json.dumps({'icao': icao,
+                                    'type': type,
+                                    'name': name,
+                                    'lat': lat,
+                                    'lon': lon,
+                                    'elevation': elevation,
+                                    'country': country,
+                                    'region': region}), content_type="application/json")
 
 
 def index(request):
     template = 'nearestAirportApp/index.html'
     return render(request, template)
-
-# def index(request):
-#      first_50_airports = Airportslist.objects.all()[:50]
-#      context = {'first_50_airports': first_50_airports}
-#      return render(request, 'nearestAirportApp/index.html', context)
-
-
-# def detail(request, icao):
-#     first_50_airports = Airportslist.objects.all()[:50]
-#     template = loader.get_template('nearestAirportApp/index.html')
-#     print(first_50_airports[0].lat - 10)
-#     context = {
-#         'first_50_airports': first_50_airports,
-#     }
-#     return HttpResponse(template.render(context, request))
-#
-# # def detail(request, icao):
-# #     return HttpResponse("You're looking at airport: %s." % icao)
-#
-#
-# def tasks_json(request):
-#     tasks = Task.objects.all()
-#     data = serializers.serialize("json", tasks)
-#     return HttpResponse(data, content_type='application/json')
